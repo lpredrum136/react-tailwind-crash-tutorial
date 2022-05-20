e1e9c2f3fc62529f64aea41ad1da7395bc1d48a2
Henry Nguyen <nguyenhai1591@gmail.com>
2022-05-20 22:38:57 +1000

    doen

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,5 @@
-import { BiHomeAlt, BiInfoCircle, BiMoviePlay } from 'react-icons/bi'
+import { useState } from 'react'
+import { BiHomeAlt, BiInfoCircle, BiMenu, BiMoviePlay } from 'react-icons/bi'
 import NavItem from './NavItem'
 
 const defaultIconSize = '1.875rem'
@@ -18,15 +19,24 @@ const NavItemsContainer = () => (
 )
 
 const Index = () => {
+	const [isNavMenuMobileOpen, setIsNavMenuMobileOpen] = useState(false)
+
 	return (
 		<nav className='col-span-1 bg-cyan-200'>
-			<div className='mx-4 justify-between items-center'>
+			<div className='flex mx-4 justify-between items-center md:block'>
 				<h4 className='uppercase font-bold text-primary py-4 border-b border-primary text-right'>
 					Phimmoi.net
 				</h4>
+				<BiMenu
+					className='cursor-pointer md:hidden'
+					size={defaultIconSize}
+					onClick={() => setIsNavMenuMobileOpen(!isNavMenuMobileOpen)}
+				/>
 			</div>
 
-			<ul className='mx-4 my-2'>
+			<ul
+				className={`mx-4 my-2${isNavMenuMobileOpen ? '' : ' hidden'} md:block`}
+			>
 				<NavItemsContainer />
 			</ul>
 		</nav>
